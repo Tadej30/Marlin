3b4b0d694766771b586551afe8d8cd2fd5d12835
Tadej30 <tadejsprem@gmail.com>
2020-10-04 10:33:25 +0200

    Update pins_BTT_SKR_PRO_common.h

--- Marlin/src/pins/stm32f4/pins_BTT_SKR_PRO_common.h
+++ Marlin/src/pins/stm32f4/pins_BTT_SKR_PRO_common.h
@@ -189,32 +189,7 @@
   //#define E2_HARDWARE_SERIAL Serial1
   //#define E3_HARDWARE_SERIAL Serial1
   //#define E4_HARDWARE_SERIAL Serial1
-
-  //
-  // Software serial
-  //
-  #define X_SERIAL_TX_PIN                   PC13
-  #define X_SERIAL_RX_PIN                   PC13
-
-  #define Y_SERIAL_TX_PIN                   PE3
-  #define Y_SERIAL_RX_PIN                   PE3
-
-  #define Z_SERIAL_TX_PIN                   PE1
-  #define Z_SERIAL_RX_PIN                   PE1
-
-  #define E0_SERIAL_TX_PIN                  PD4
-  #define E0_SERIAL_RX_PIN                  PD4
-
-  #define E1_SERIAL_TX_PIN                  PD1
-  #define E1_SERIAL_RX_PIN                  PD1
-
-  #define E2_SERIAL_TX_PIN                  PD6
-  #define E2_SERIAL_RX_PIN                  PD6
-
-  // Reduce baud rate to improve software serial reliability
-  #define TMC_BAUD_RATE                    19200
-#endif
-
+  
 //
 // Software serial
 //
