6e370b29eafcd0b4cbd68d0eddd5a7e46b2144e8
Tadej30 <tadejsprem@gmail.com>
2021-02-10 20:44:16 +0100

    c

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -21,102 +21,56 @@
  */
 #pragma once
 
-/**
- * Release version. Leave the Marlin version or apply a custom scheme.
- */
-#ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "2.0.7.x"
-#endif
+////////////////////////////
+// VENDOR VERSION EXAMPLE //
+////////////////////////////
 
 /**
- * Verbose version identifier containing a unique identifier, such as the
- * vendor name, download location, GitHub account, etc.
+ * Marlin release version identifier
  */
-#ifndef DETAILED_BUILD_VERSION
-  #define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION
-#endif
+#define SHORT_BUILD_VERSION "2.0.7.2"
+
+/**
+ * Verbose version identifier which should contain a reference to the location
+ * from where the binary was downloaded or the source code was compiled.
+ */
+//#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION
 
 /**
  * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "10.2.2021"
-#endif
+#define STRING_DISTRIBUTION_DATE "8.2.2021"
 
 /**
- * Minimum Configuration.h and Configuration_adv.h file versions.
- * Set based on the release version number. Used to catch an attempt to use
- * older configurations. Override these if using a custom versioning scheme
- * to alert users to major changes.
+ * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-
-#define MARLIN_HEX_VERSION 020008
-#ifndef REQUIRED_CONFIGURATION_H_VERSION
-  #define REQUIRED_CONFIGURATION_H_VERSION MARLIN_HEX_VERSION
-#endif
-#ifndef REQUIRED_CONFIGURATION_ADV_H_VERSION
-  #define REQUIRED_CONFIGURATION_ADV_H_VERSION MARLIN_HEX_VERSION
-#endif
+#define MACHINE_NAME "Prusa MK3/s"
 
 /**
- * The protocol for communication to the host. Protocol indicates communication
- * standards such as the use of ASCII, "echo:" and "error:" line prefixes, etc.
- * (Other behaviors are given by the firmware version and capabilities report.)
+ * The SOURCE_CODE_URL is the location where users will find the Marlin Source
+ * Code which is installed on the device. In most cases —unless the manufacturer
+ * has a distinct Github fork— the Source Code URL should just be the main
+ * Marlin repository.
  */
-#ifndef PROTOCOL_VERSION
-  #define PROTOCOL_VERSION "1.0"
-#endif
-
-/**
- * Define a generic printer name to be output to the LCD after booting Marlin.
- */
-#ifndef MACHINE_NAME
-  #define MACHINE_NAME "Prusa MK3/s"
-#endif
-
-/**
- * Website where users can find Marlin source code for the binary installed on the
- * device. Override this if you provide public source code download. (GPLv3 requires
- * providing the source code to your customers.)
- */
-#ifndef SOURCE_CODE_URL
-  #define SOURCE_CODE_URL "https://github.com/Tadej30/Marlin/tree/Prusa_MK3/s"
-#endif
+#define SOURCE_CODE_URL "https://github.com/Tadej30/Marlin/tree/My_Marlin_2.0.x"
 
 /**
  * Default generic printer UUID.
  */
-#ifndef DEFAULT_MACHINE_UUID
-  #define DEFAULT_MACHINE_UUID "165f2846-12b6-4c04-8592-80f86a3e7b5d"
-#endif
+#define DEFAULT_MACHINE_UUID "165f2846-12b6-4c04-8592-80f86a3e7b5d"
 
-  /**
-   * The WEBSITE_URL is the location where users can get more information such as
-   * documentation about a specific Marlin release. Displayed in the Info Menu.
-   */
-#ifndef WEBSITE_URL
-  #define WEBSITE_URL "marlinfw.org"
-#endif
+/**
+ * The WEBSITE_URL is the location where users can get more information such as
+ * documentation about a specific Marlin release.
+ */
+#define WEBSITE_URL "marlinfw.org"
 
 /**
  * Set the vendor info the serial USB interface, if changable
  * Currently only supported by DUE platform
  */
-#ifndef USB_DEVICE_VENDOR_ID
-  #define  USB_DEVICE_VENDOR_ID           0x03EB /* ATMEL VID */
-#endif
-#ifndef USB_DEVICE_PRODUCT_ID
-  #define  USB_DEVICE_PRODUCT_ID          0x2424 /* MSC / CDC */
-#endif
-//! USB Device string definitions (Optional)
-#ifndef USB_DEVICE_MANUFACTURE_NAME
-  #define  USB_DEVICE_MANUFACTURE_NAME    WEBSITE_URL
-#endif
-#ifdef CUSTOM_MACHINE_NAME
-  #define  USB_DEVICE_PRODUCT_NAME        CUSTOM_MACHINE_NAME
-#else
-  #define  USB_DEVICE_PRODUCT_NAME        MACHINE_NAME
-#endif
-#define  USB_DEVICE_SERIAL_NAME           "123985739853"
+//#define USB_DEVICE_VENDOR_ID           0x0000
+//#define USB_DEVICE_PRODUCT_ID          0x0000
+//#define USB_DEVICE_MANUFACTURE_NAME    WEBSITE_URL
